24888761c27d895528a4a1cc0e4cb3d4eb3aee27
keyhit <viacheslavkarpenkoju@gmail.com>
2019-01-03 00:55:47 +0200

    Continue searching products for admin page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,25 @@
 class ProductsController < ApplicationController
   def products_by
     @categories ||= Category.all
+    if params[:commit] =='Show'
+      unless params[:price_range] == 'Select range' && params[:category] == "Select category"
+        range = params[:price_range].gsub(/ - /, ' ').split(' ')
+        @products_by = Product.where('price BETWEEN ? AND ? AND category_id  = ?', range[0], range[1], params[:category])
+        @products_count = @products_by.count
+      end
 
-  unless params[:price_range] == "Select range" && params[:category] == "Select category"
-    range = params[:price_range].gsub(/ - /, ' ').split(' ')
-    @products_by = Product.where('price BETWEEN ? AND ? AND category_id  = ? ', range[0], range[1], params[:category] )
-    @products_count = @products_by.count
-  end
+      if params[:category] == 'Select category' && params[:price_range] != 'Select range'
+        range = params[:price_range].gsub(/ - /, ' ').split(' ')
+        @products_by = Product.where('price BETWEEN ? AND ?', range[0], range[1])
+        @products_count = @products_by.count
+        @range = 'range_selector'
+      end
 
-  unless params[:price_range] == "Select range"
-    range = params[:price_range].gsub(/ - /, ' ').split(' ')
-    @products_by = Product.where('price BETWEEN ? AND ? ', range[0], range[1])
-    @products_count = @products_by.count
-    @range = "range_selector"
-  end
-
-    unless params[:category] == 'Select category'
-      @products_by = Category.find(params[:category]).products
-      @products_count = @products_by.count
-      @cat = 'category_selector'
+      if params[:price_range] == 'Select range' && params[:category] != 'Select category'
+        @products_by = Category.find(params[:category]).products
+        @products_count = @products_by.count
+        @cat = 'category_selector'
+      end
     end
 
 # binding.pry
